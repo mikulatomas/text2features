cf1b98fa3b37377bb4462f9695d5c775236bf641
Tomáš Mikula <mail@tomasmikula.cz>
2019-12-29 15:21:54 +0100

    Fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(packages=find_packages(),
       name='text2context',
-      description='Extracts keywords from text files to boolean datasets.'
+      description='Extracts keywords from text files to boolean datasets.',
       version='0.0.1',
       author='Tomáš Mikula',
       author_email='mail@tomasmikula.cz',
